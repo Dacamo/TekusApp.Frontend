0230880b41e6c5487c89898810955a2fdb7742c0
Dacamo <castrom1998@gmail.com>
2020-07-08 00:53:58 -0500

    Se agregan dependencias y se configura redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, compose } from 'redux';
+import Root from './components/Root';
+import thunk from 'redux-thunk';
+import reducers from './reducers/';
+import './common/overrides.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const composeEnhancers= window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, 
+    composeEnhancers(
+    applyMiddleware(thunk)))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDom.render(
+    <Provider store={store}>
+        <Root store={store} />
+    </Provider>,
+    document.querySelector('#root'));
